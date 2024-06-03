8213bb8658bebc7981b6a09fcc3e8a9c0f622df3
Vladislav-zalozniy-qa <Arzes-vlad@yandex.ru>
2024-06-03 12:35:12 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Rest API, SOAP, Postman, SQL & NoSQL, Kafka, Kibana, Sentry, Grafana, Firebase, Jaeger, Jmeter, Cypress, Selenium, Docker, Charles, Swagger')
+})
